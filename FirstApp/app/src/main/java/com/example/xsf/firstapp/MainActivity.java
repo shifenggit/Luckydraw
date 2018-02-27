b1922e5b504a675dbdb0309b09933201b8cd95fd
shifenggit <36726117+shifenggit@users.noreply.github.com>
2018-02-27 18:03:42 +0800

    增加动画和提示
    抽签结果增加动画效果和Toast提示

--- FirstApp/app/src/main/java/com/example/xsf/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/example/xsf/firstapp/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import java.util.Random;
+import android.animation.ObjectAnimator;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
     // 声明对象
@@ -46,6 +48,8 @@ public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     public void onClick(View v) {
+        ObjectAnimator animator = null;
+
         switch (v.getId()) {
             case R.id.radioButton:
                 rbt_2.setChecked(false);
@@ -82,8 +86,65 @@ public class MainActivity extends Activity implements OnClickListener {
             case R.id.pre:
                 image.setVisibility(View.VISIBLE);
                 nub = rand.nextInt(N);
-                image.setImageResource(images[nub]);
-                break;
+                    switch (nub) {
+                        case 0:
+                            image.setImageResource(images[0]);
+                            // 沿X轴向移动100px，然后向左移动回到原位
+                            // 沿Y轴移动，ofFloat第二个参数传入translationY
+                            animator = ObjectAnimator.ofFloat(image, "translationY", 0f, 100f, 0f);
+                            Toast toast = Toast.makeText (MainActivity.this, "抽到1", Toast.LENGTH_SHORT);
+                            toast.show();
+                            break;
+
+                        case 1:
+                            image.setImageResource(images[1]);
+                            // 垂直旋转360度
+                            // 水平旋转，ofFloat第二个参数传入rotationX
+                            animator = ObjectAnimator.ofFloat(image, "rotationY", 0f, 360f);
+                            Toast toast1 = Toast.makeText (MainActivity.this, "抽到2", Toast.LENGTH_SHORT);
+                            toast1.show();
+                            break;
+
+                        case 2:
+                            image.setImageResource(images[2]);
+                            // 透明度从0到1
+                            animator = ObjectAnimator.ofFloat(image, "alpha", 0f, 1f);
+                            Toast toast2 = Toast.makeText (MainActivity.this, "抽到3", Toast.LENGTH_SHORT);
+                            toast2.show();
+                            break;
+
+                        case 3:
+                            image.setImageResource(images[3]);
+                            // 水平缩放0.5倍，然后恢复
+                            // 垂直缩放，ofFloat第二个参数传入scaleY
+                            animator = ObjectAnimator.ofFloat(image, "scaleX", 1f, 0.5f, 1f);
+                            Toast toast3 = Toast.makeText (MainActivity.this, "抽到4", Toast.LENGTH_SHORT);
+                            toast3.show();
+                            break;
+
+                        case 4:
+                            image.setImageResource(images[4]);
+                            // 垂直旋转360度
+                            // 水平旋转，ofFloat第二个参数传入rotationX
+                            animator = ObjectAnimator.ofFloat(image, "rotationY", 0f, 360f);
+                            Toast toast4 = Toast.makeText (MainActivity.this, "抽到5", Toast.LENGTH_SHORT);
+                            toast4.show();
+                            break;
+
+                        case 5:
+                            image.setImageResource(images[5]);
+                            // 水平缩放0.5倍，然后恢复
+                            // 垂直缩放，ofFloat第二个参数传入scaleY
+                            animator = ObjectAnimator.ofFloat(image, "scaleX", 1f, 0.5f, 1f);
+                            Toast toast5 = Toast.makeText (MainActivity.this, "抽到6", Toast.LENGTH_SHORT);
+                            toast5.show();
+                            break;
+                    }
+        }
+        if (animator != null) {
+            // 动画执行时长2s
+            animator.setDuration(1000);
+            animator.start();
         }
     }
 
